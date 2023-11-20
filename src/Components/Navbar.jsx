89dcd6c9252e64c75abaa3c087120e46e86576bb
Fernando Díaz <feeldiac@gmail.com>
2023-11-19 23:41:09 -0500

    feat: style navbar including dark mode

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,41 @@
-import React from 'react'
-import { useContext } from "react";
+import React, { useContext } from "react";
+import { Link } from "react-router-dom";
+import { Moon, Sun } from "../assets";
 import { GlobalContext } from "../contexts/GlobalContext";
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Navbar = () => {
   const { theme, darken, lighten } = useContext(GlobalContext);
-  const toggleTheme = () => theme === 'light' ? darken() : lighten();
+  const toggleTheme = () => (theme === "light" ? darken() : lighten());
 
   return (
-    <nav>
-      {/* Aqui deberan agregar los liks correspondientes a las rutas definidas */}
-      {/* Deberan implementar ademas la logica para cambiar de Theme con el button */}
-      <button onClick={toggleTheme}>Change theme</button>
+    <nav className={theme === "dark" && "dark-mode"}>
+      <h1 className="logo">Pearly White Hub</h1>
+      <div className="nav-items">
+        <ul className="nav-links">
+          <li className="nav-link">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-link">
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li className="nav-link">
+            <Link to="/favs">Favorites</Link>
+          </li>
+        </ul>
+        <span className="theme-button" onClick={toggleTheme}>
+          {theme === "light" ? (
+            <Moon fill="palevioletred" />
+          ) : (
+            <Sun fill="white" />
+          )}
+          {/* <img
+            src={theme === "light" ? iconSun : iconMoon}
+            alt="toggle-theme"
+          /> */}
+        </span>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;

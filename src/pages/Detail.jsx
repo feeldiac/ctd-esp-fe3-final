4382f54a817490b9cc5a14022e5ab2fe46c8fda5
Fernando Díaz <feeldiac@gmail.com>
2023-11-20 23:45:16 -0500

    feat: finish Detail component

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,22 +1,30 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-import { useGetDentist } from '../hooks'
-
-//Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useGetDentist } from "../hooks";
 
 const Detail = () => {
   const { id } = useParams();
   const { data } = useGetDentist(id);
-  console.log(data);
-  // Consumiendo el parametro dinamico de la URL deberan hacer un fetch a un user en especifico
 
   return (
-    <>
-      <h1>Detail Dentist id </h1>
-      {/* aqui deberan renderizar la informacion en detalle de un user en especifico */}
-      {/* Deberan mostrar el name - email - phone - website por cada user en especifico */}
-    </>
-  )
-}
+    <main className="main" style={{ display: "flex", flexDirection: "column" }}>
+      <h1 className="title">
+        This is <em>{data?.name}'s</em> bio!{" "}
+      </h1>
+      <div className="detail-content">
+        <p>
+          His/Her email is <br /> <em>{data?.email?.toLowerCase()}</em>
+        </p>
+        <p>
+          You can reach him/her at <br /> <em>{data?.phone}</em>
+        </p>
+        <p>
+          You can also find him/her at <br /> <em>{data?.website}</em>
+        </p>
+        <p>We are proud of our professionals!</p>
+      </div>
+    </main>
+  );
+};
 
-export default Detail
+export default Detail;
